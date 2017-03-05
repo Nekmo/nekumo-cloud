4ace8613d26eb05aeb12ceb1099c5ac34d99893d
Nekmo <contacto@nekmo.com>
2017-03-05 02:45:06 +0100

    Views and catch ENOSPC.

--- nekumo/gateways/fs/watcher.py
+++ nekumo/gateways/fs/watcher.py
@@ -1,8 +1,12 @@
+import logging
 import os
 
 import pyinotify as pyinotify
 
 from nekumo.core.events import GatewayUpdateEvent
+
+
+logger = logging.getLogger('nekumo')
 
 
 class EventHandler(pyinotify.ProcessEvent):
@@ -25,4 +29,7 @@ def init_watcher(iface):
     notifier.daemon = True
     notifier.start()
     mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE  # watched events
-    wdd = wm.add_watch(iface.get_root_path(), mask, rec=True)
+    try:
+        wdd = wm.add_watch(iface.get_root_path(), mask, rec=True, quiet=False)
+    except pyinotify.WatchManagerError as e:
+        logger.warning('Watcher not available for {}: {}'.format(iface, e))
